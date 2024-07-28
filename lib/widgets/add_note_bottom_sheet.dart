2ce961813bf1aa5725e4f961b975d81a5a41940b
YNinja1 <sofaelpro@gmail.com>
2024-07-28 15:12:55 +0300

    creating CustomTextField and primaryColor

--- lib/widgets/add_note_bottom_sheet.dart
+++ lib/widgets/add_note_bottom_sheet.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:note_app/widgets/custom_text_field.dart';
 
 class AddNoteBottomSheet extends StatelessWidget {
   const AddNoteBottomSheet({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return  Container();
+    return const Padding(
+      padding:  EdgeInsets.all(16),
+      child: Column(
+        children: [
+          SizedBox(height: 16,
+          ),
+      CustomTextField(),
+        ],
+      ),
+    );
   }
 }
