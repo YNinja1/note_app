7bbdcd04b38ae012f65c3627e9627f7b73cc35c8
YNinja1 <sofaelpro@gmail.com>
2024-08-05 14:57:50 +0300

    creating fetchNote method + providing ShowNote cubit

--- lib/cubits/show_notes_cubit/show_notes_cubit.dart
+++ lib/cubits/show_notes_cubit/show_notes_cubit.dart
@@ -1,9 +1,23 @@
-import 'package:bloc/bloc.dart';
-import 'package:meta/meta.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hive_flutter/adapters.dart';
+import 'package:note_app/constants.dart';
 import 'package:note_app/models/note_model.dart';
 
 part 'show_notes_state.dart';
 
 class ShowNotesCubit extends Cubit<ShowNotesState> {
   ShowNotesCubit() : super(ShowNotesInitial());
+
+  fetchAllNotes() {
+    try{
+     var notesBox = Hive.box<NoteModel>(kNotesBox);
+     
+    List<NoteModel> notes = notesBox.values.toList();
+    emit(ShowNotesSuccess(notes));
+    }catch(e)
+    {
+      emit(ShowNotesFailure(e.toString()));
+    }
+  }
 }
