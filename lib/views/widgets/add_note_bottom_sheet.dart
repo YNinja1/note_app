b07c6177ff339ba9a4c14f410172e7a17a762590
YNinja1 <sofaelpro@gmail.com>
2024-08-08 16:33:30 +0300

    refreshing notesList

--- lib/views/widgets/add_note_bottom_sheet.dart
+++ lib/views/widgets/add_note_bottom_sheet.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:note_app/cubits/add_note_cubit/add_note_cubit.dart';
+import 'package:note_app/cubits/display_note_cubit/display_note_cubit.dart';
 import 'package:note_app/views/widgets/add_note_form.dart';
 
 class AddNoteBottomSheet extends StatelessWidget {
@@ -16,6 +17,7 @@ class AddNoteBottomSheet extends StatelessWidget {
             print(' failed ${state.errMessage}');
           }
           if (state is AddNoteSuccess) {
+            BlocProvider.of<DisplayNoteCubit>(context).fetchAllNotes();
             Navigator.pop(context);
           }
         },
